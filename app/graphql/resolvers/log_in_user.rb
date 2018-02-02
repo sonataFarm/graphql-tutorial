2575dcccfddf160f455eb0b38be9edc7fec79faa
Nate Festinger <nate.festinger@gmail.com>
2018-02-02 09:35:20 -0800

    Add session token to User auth

--- app/graphql/resolvers/log_in_user.rb
+++ app/graphql/resolvers/log_in_user.rb
@@ -8,7 +8,7 @@ class Resolvers::LogInUser < GraphQL::Function
     field :user, Types::UserType
   end
 
-  def call(_obj, args, _ctx)
+  def call(_obj, args, ctx)
     input = args[:login_info]
     return unless input
 
@@ -16,9 +16,11 @@ class Resolvers::LogInUser < GraphQL::Function
 
     return unless user
     return unless user.authenticate(input[:password])
-    
+
     crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base.byteslice(0..31))
     token = crypt.encrypt_and_sign("user-id:#{user.id}")
+    ctx[:session][:token] = token
+    
     OpenStruct.new({
       user: user,
       token: token
