e0332ee742f7ba190b3d929e72bd5c14d7b8bd12
Nate Festinger <nate.festinger@gmail.com>
2018-02-23 19:47:41 -0800

    Add Links and GraphQL functionality

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,8 @@
+Types::QueryType = GraphQL::ObjectType.define do
+  name 'Query'
+
+  # queries are just represented as fields
+  field :allLinks, !types[Types::LinkType] do
+    resolve -> (obj, args, ctx) { Link.all }
+  end
+end
